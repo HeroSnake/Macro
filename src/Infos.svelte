56cc9fa1ffffdbe07d9b1424402dbdb61e103af2
Florent SYX <florent.syx@outlook.fr>
2021-11-01 18:15:23 +0100

    display fixes compare

--- src/Infos.svelte
+++ src/Infos.svelte
@@ -3,5 +3,10 @@
 </script>
 
 <span>
-    <b>{result.data.calories}</b> Cal - <b>{result.data.totalWeight}</b> g
+    {#if isPhone}
+        <b>{result.data.calories}</b> Cal<br>
+        <b>{result.data.totalWeight}</b> g
+    {:else}
+        <b>{result.data.calories}</b> Cal - <b>{result.data.totalWeight}</b> g
+    {/if}
 </span>
